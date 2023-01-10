101bb728df350757bb68d99c5ac6a52ed9cfb383
Yosteven <yosteven.yosteven@adira.co.id>
2023-01-11 01:50:35 +0700

    Deeplink NAtive Android

---
+++ app/src/main/java/com/example/deeplinknative/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.deeplinknative
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val action: String? = intent?.action
+        val data: Uri? = intent?.data
+
+        Log.d(action, "actions: ")
+        Log.d(data.toString(), "uri: ")
+    }
+}
